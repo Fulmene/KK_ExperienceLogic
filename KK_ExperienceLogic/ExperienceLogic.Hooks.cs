0ed687a42a929c3a0758c49a1b67caa5df03f557
Ada Joule <ada.fulmina@gmail.com>
2019-04-20 00:04:00 +0700

    Add code logic

---
+++ KK_ExperienceLogic/ExperienceLogic.Hooks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace KK_ExperienceLogic
+{
+    public partial class ExperienceLogic
+    {
+        private class Hooks
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(SaveData.Heroine))]
+            [HarmonyPatch("HExperience", PropertyMethod.Getter)]
+            public static void GetHExperiencePost(SaveData.Heroine __instance, ref SaveData.Heroine.HExperienceKind __result)
+            {
+                if (__result == SaveData.Heroine.HExperienceKind.不慣れ)
+                {
+                    float caressBreasts = __instance.hAreaExps[1];
+                    float caressVagina = __instance.hAreaExps[2];
+                    float caressAnus = __instance.hAreaExps[3];
+                    float caressButt = __instance.hAreaExps[4];
+                    float caressNipple = __instance.hAreaExps[5];
+                    float service = __instance.houshiExp;
+                    float pistonVagina = __instance.countKokanH;
+                    float pistonAnus = __instance.countAnalH;
+
+                    if (caressBreasts >= 100f && caressButt >= 100f && caressNipple >= 100f && service >= 100f)
+                    {
+                        if ((caressVagina >= 100f && pistonVagina >= 100f) || (caressAnus >= 100f && pistonAnus >= 100f))
+                        {
+                            if (__instance.lewdness >= 100)
+                                __result = SaveData.Heroine.HExperienceKind.淫乱;
+                            else
+                                __result = SaveData.Heroine.HExperienceKind.慣れ;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KK_ExperienceLogic/ExperienceLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using Harmony;
+
+namespace KK_ExperienceLogic
+{
+    [BepInProcess("Koikatu")]
+    [BepInPlugin(GUID, Name, Version)]
+    public partial class ExperienceLogic : BaseUnityPlugin
+    {
+        public const string GUID = "fulmene.experiencelogic";
+        public const string Name = "Koikatsu Experience Logic";
+        internal const string Version = "1.0";
+
+        [DisplayName("Need to touch only one hole")]
+        [Description("Make it so you only need to max the girls' either vaginal caress/piston or anal caress/piston,\n" +
+                     "instead of both, to achieve experienced state.")]
+        public static ConfigWrapper<bool> SingleHole { get; set; }
+
+        public ExperienceLogic()
+        {
+            SingleHole = new ConfigWrapper<bool>("SingleHole", this, true);
+
+            var harmony = HarmonyInstance.Create(GUID);
+
+            if (SingleHole.Value)
+                harmony.PatchAll(typeof(ExperienceLogic.Hooks));
+        }
+
+    }
+}
